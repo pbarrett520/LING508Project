f3fd259956712ee5906b6a98b564b1e75f88ede9
pbarrett520 <pbarrett520@yahoo.com>
2023-07-30 18:54:59 +0800

    Not really happy with what I have but I suppose it checks the boxes

--- tests/test_data.py
+++ tests/test_data.py
@@ -10,3 +10,5 @@ def test_data():
     assert sql_test.get_entries() != None
     assert isinstance(entries[3][0],int)
     assert sample_code_point in range(cjk_range_min, cjk_range_max)
+# I recognize this should be a bit more complex, but I spent way too long debugging the web scraper script and playing with the sql to do more than this.
+# Also my life has been consumed by packing as I prepare to move to the US next month
